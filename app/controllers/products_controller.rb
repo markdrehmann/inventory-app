6b71230f0c0b85e679cd3309a27ea4724cc67022
markdrehmann <markdrehmann@gmail.com>
2022-01-17 21:27:01 -0800

    Adding to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,26 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+
+  end
+
+  def Edit
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :quantity)
+  end
 end
